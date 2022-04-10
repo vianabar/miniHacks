e23f0136104334b1ae87318a65b2279ef5a0b172
elisa3lopes <elisa3lopes@gmail.com>
2022-04-09 23:24:42 -0400

    adding updated test train file

--- 1_data_manipulation.py
+++ 1_data_manipulation.py
@@ -4,44 +4,54 @@ import pandas as pd
 df_train = pd.read_csv('train.csv')
 df_test = pd.read_csv('test.csv')
 
+datasets = [df_train, df_test]
+for i in range(len(datasets)):
+
+    # Yes or No binary ones : 1 or 0
+    # schoolsup, famsup, paid, activities, nursery, higher, internet, romantic (17 to 24)
+    datasets[i]= datasets[i].replace('yes', 1)
+    datasets[i] = datasets[i].replace('no', 0)
+
+    # school: 'GP' to 1 and 'MS' to 0
+    datasets[i] = datasets[i].replace('GP', 1)
+    datasets[i] = datasets[i].replace('MS', 0)
+
+    # sex: 'F' to 1 and 'M' to 0
+    datasets[i] = datasets[i].replace('F', 1)
+    datasets[i] = datasets[i].replace('M', 0)
+
+    # address: 'U' to 1 and 'R' to 0
+    datasets[i] = datasets[i].replace('U', 1)
+    datasets[i] = datasets[i].replace('R', 0)
+
+    # famsize: 'LE3' to 1 and 'GT3' to 0
+    datasets[i] = datasets[i].replace('LE3', 1)
+    datasets[i] = datasets[i].replace('GT3', 0)
+
+    # Pstatus: 'T' to 1 and 'A' to 0
+    datasets[i] = datasets[i].replace('T', 1)
+    datasets[i] = datasets[i].replace('A', 0)
+
+    #% Nominal
+    # Mjob/Fjob: 'teacher', 'health', 'services', 'at_home', 'other' to 0-4
+    datasets[i] = datasets[i].replace('teacher', 0)
+    datasets[i] = datasets[i].replace('health', 1)
+    datasets[i] = datasets[i].replace('services', 2)
+    datasets[i] = datasets[i].replace('at_home', 3)
+    datasets[i] = datasets[i].replace( value = 4, to_replace = {'Mjob':'other','Fjob':'other'})
+
+    # reason: 'home', 'reputation', 'course', 'other' to 0-3
+    datasets[i] = datasets[i].replace('home', 0)
+    datasets[i] = datasets[i].replace('reputation', 1)
+    datasets[i] = datasets[i].replace('course', 2)
+    datasets[i] = datasets[i].replace( value=3, to_replace={'reason':'other'})
+
+    # guardian: 'mother', 'father', 'other' to 0-2
+    datasets[i] = datasets[i].replace('mother', 0)
+    datasets[i] = datasets[i].replace('father', 1)
+    datasets[i] = datasets[i].replace( value=2, to_replace={'guardian':'other'})
 
 
-# Yes or No binary ones : 1 or 0
-# schoolsup, famsup, paid, activities, nursery, higher, internet, romantic (17 to 24)
-df_train = df_train.replace('yes', 1)
-df_train = df_train.replace('no', 0)
-df_test = df_test.replace('yes', 1)
-df_test = df_test.replace('no', 0)
 
-# sex: 'F' to 1 and 'M' to 0
-df_train = df_train.replace('F', 1)
-df_train = df_train.replace('M', 0)
-df_test = df_test.replace('F', 1)
-df_test = df_test.replace('M', 0)
-
-# address: 'U' to 1 and 'R' to 0
-df_train = df_train.replace('U', 1)
-df_train = df_train.replace('R', 0)
-df_test = df_test.replace('U', 1)
-df_test = df_test.replace('R', 0)
-
-# famsize: 'LE3' to 1 and 'GT3' to 0
-df_train = df_train.replace('LE3', 1)
-df_train = df_train.replace('GT3', 0)
-df_test = df_test.replace('LE3', 1)
-df_test = df_test.replace('GT3', 0)
-
-# Pstatus: 'T' to 1 and 'A' to 0
-df_train = df_train.replace('T', 1)
-df_train = df_train.replace('A', 0)
-df_test = df_test.replace('T', 1)
-df_test = df_test.replace('A', 0)
-
-# school: 'GP' to 1 and 'MS' to 0
-df_train = df_train.replace('GP', 1)
-df_train = df_train.replace('MS', 0)
-df_test = df_test.replace('GP', 1)
-df_test = df_test.replace('MS', 0)
-
-df_train.to_csv('train_new.csv')  
-df_test.to_csv('test_new.csv')  
+datasets[0].to_csv('train_new.csv')  
+datasets[1].to_csv('test_new.csv')  
