9dadb9871fcec22eb582572bef0b201d3c88f3b2
vianabar <viana@bu.edu>
2022-04-09 23:47:35 -0400

    added boxplots for each feature against grade

---
+++ 2_analysing_features_1.py
@@ -0,0 +1,30 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_numeric(dataset, feature):
+
+    sns.set_theme(style="whitegrid")
+    
+    # ax = sns.boxplot(x=feature, y="grade", data=dataset)
+    # ax.title = "Boxplot of " + feature + " vs. grade"
+    # png_title = "boxplot_" + feature + "_vs_grade"
+    # ax.savefig("/boxplot_figs/" + png_title)\
+
+    sns.boxplot(x=feature, y="grade", data=dataset)
+    plt.title("Boxplot of " + feature + " vs. grade")
+    png_title = "boxplot_" + feature + "_vs_grade"
+    plt.savefig(png_title)
+    print(feature,end='\r')
+
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+    
+# %%
+for feature in train:
+    plot_numeric(train, feature)
+
+
---
+++ plot_numerics.py
@@ -0,0 +1,19 @@
+import seaborn as sn
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+import pandas as pd
+import pickle
+import h5py
+import sklearn
+import sys
+
+from csv import reader
+
+    
+
+with open('train.csv', 'r') read_obj:
+    csv_reader = reader(read_ob)
+
+    for row in csv_reader:
+
