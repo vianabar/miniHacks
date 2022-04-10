cf972a5f2c6ff2bb79e3e862f62580417a794413
elisa3lopes <elisa3lopes@gmail.com>
2022-04-09 22:06:39 -0400

    adding initial files

---
+++ 1_data_manipulation.py
@@ -0,0 +1,12 @@
+# transform 
+# 
+# sex: 'F' to 1 and 'M' to 0
+# address: 'U' to 1 and 'R' to 0
+# famsize: 'LE3' to 1 and 'GT3' to 0
+# Pstatus: 'T' to 1 and 'A' to 0
+
+
+# Yes or No binary ones : 1 or 0
+# schoolsup, famsup, paid, activities, nursery, higher, internet, romantic (17 to 24)
+
+
